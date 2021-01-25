25c4dd9a83933904eb5cdb28682246ab7bd60be4
Aaron <aaronmarsh@live.co.uk>
2021-01-25 22:12:18 +0000

    starting the file with python imports

---
+++ scraper.py
@@ -0,0 +1,10 @@
+import csv
+from getpass import getpass
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Firefox
+
+
+Firefox.get("https://www.google.com")
+
